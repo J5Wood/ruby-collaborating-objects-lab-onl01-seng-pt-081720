37978a3371ffc5f14b014c3fc42e2f63eb0a22a0
Jeremy Wood <js.wood206@gmail.com>
2020-09-13 04:58:54 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+require 'pry'
+
+
+class Artist
+
+  attr_accessor :name, :artist, :songs
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def add_song(song)
+    song.artist = self.name
+  end
+
+
+  def songs
+    Song.all.select { |x| x.artist == self.name}
+  end
+
+
+  def self.find_or_create_by_name(name)
+    if self.all.find { |x| x.name == name }
+      self.all.find { |x| x.name == name }
+    else
+      name = self.new(name)
+      name
+    end
+  end
+
+
+  def print_songs
+    self.songs.each { |x| puts x.name }
+  end
+
+
+
+end
